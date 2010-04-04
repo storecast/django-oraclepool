cf91a2951ea1bf7dea7c765998ef2740e5b713e7
Ed Crewe <ed.crewe@bristol.ac.uk>
2010-04-04 23:48:00 +0100

    need to use rst extension for bit bucket so update in setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '0.5'
 setup(name='django-oraclepool',
       version=version,
       description="django database backend that uses cx_Oracle session pooling for connections",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "TODO.txt")).read() + "\n" +      
                        open(os.path.join("docs", "HISTORY.txt")).read(),      
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
