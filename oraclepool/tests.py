f86aeb1119f726290b797bf182f9e7ea2bdff205
Ed Crewe <ed.crewe@bris.ac.uk>
2012-10-17 23:35:23 +0100

    add documentation to tests

--- oraclepool/tests.py
+++ oraclepool/tests.py
@@ -1,3 +1,15 @@
-# To run the tests for oraclepool
-# Go to django-oraclepool/tests
-# Run manage.py test
+print """To run the tests for oraclepool
+         Go to django-oraclepool/tests
+         Replace the details / or add a cred.py containing
+
+        CREDENTIALS = {'ENGINE' : 'oraclepool',           
+                            'USER' : 'scott',      
+                            'NAME' : 'scott',             
+                            'PASSWORD' : 'tiger',     
+                            'HOST' : 'example.com',
+                            'PORT' : '1521'            
+                           }
+
+         Run manage.py test
+         Or if you are using virtualenv use 
+         bin/python src/django-oraclepool/tests/manage.py test"""
