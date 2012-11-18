1ef4421047b88c04885af7afe89fa344078f953e
Ed Crewe <ed.crewe@bris.ac.uk>
2012-11-18 17:40:35 +0000

    refactor and rm cred

--- oraclepool/tests/cred.py
+++
@@ -1,7 +0,0 @@
-CREDENTIALS = {'ENGINE' : 'oraclepool',           
-                    'USER' : 'uobcms',      
-                    'NAME' : 'diverst',             
-                    'PASSWORD' : 'goya457',     
-                    'HOST' : 'doradus.isys.bris.ac.uk', 
-                    'PORT' : '1555'            
-                   }
